07f6451762196ffeb3fc49dc0187a4825ced17ab
Manav <manavm@utexas.edu>
2016-06-03 18:20:48 -0400

    Create an Ingredient class

---
+++ FoodStock/Ingredient.swift
@@ -0,0 +1,31 @@
+//
+//  Ingredient.swift
+//  FoodStock
+//
+//  Created by Manav Mandhani on 6/3/16.
+//  Copyright © 2016 Mandhani. All rights reserved.
+//
+
+import UIKit
+
+class Ingredient {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var count: Int
+    
+    // MARK: Initialization
+    
+    init?(name:String, photo: UIImage, count: Int){
+        self.name = name
+        self.photo = photo
+        self.count = count
+    
+        if name.isEmpty{
+            return nil
+        }
+    }
+    
+    
+}
